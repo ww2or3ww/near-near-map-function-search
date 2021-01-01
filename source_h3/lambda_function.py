88cda0d4f8838869440b3d2891ac34ad698cabb4
ww2or3ww <w2or3w@gmail.com>
2021-01-01 03:27:26 +0000

    for alexa and change loco api

--- source_h3/lambda_function.py
+++ source_h3/lambda_function.py
@@ -13,6 +13,8 @@ from urllib.parse import urljoin
 from retry import retry
 import boto3
 from boto3.dynamodb.conditions import Key
+import googlemaps
+from geopy.distance import geodesic
 
 import logging
 logger = logging.getLogger()
@@ -20,20 +22,41 @@ logger.setLevel(logging.INFO)
 
 DYNAMODB_NAME               = ""    if("DYNAMODB_NAME" not in os.environ)           else os.environ["DYNAMODB_NAME"]
 LOCOGUIDE_API_ADDRESS       = ""    if("LOCOGUIDE_API_ADDRESS" not in os.environ)   else os.environ["LOCOGUIDE_API_ADDRESS"]
+LOCOGUIDE_API_ADDRESS2       = ""   if("LOCOGUIDE_API_ADDRESS2" not in os.environ)  else os.environ["LOCOGUIDE_API_ADDRESS2"]
 LOCOGUIDE_API_TOKEN         = ""    if("LOCOGUIDE_API_TOKEN" not in os.environ)     else os.environ["LOCOGUIDE_API_TOKEN"]
+APIKEY_GOOGLE_MAP           = ""    if("APIKEY_GOOGLE_MAP" not in os.environ)       else os.environ["APIKEY_GOOGLE_MAP"]
 RESULT_COUNT_MAX            = 100
 
 DYNAMO_TABLE = boto3.resource("dynamodb").Table(DYNAMODB_NAME)
+GMAPS        = googlemaps.Client(key=APIKEY_GOOGLE_MAP)
 
 def lambda_handler(event, context):
     try:
         logger.info("=== START ===")
         types = event["queryStringParameters"]["type"]
-        latlon = event["queryStringParameters"]["latlon"]
-        zoom = int(event["queryStringParameters"]["zoom"])
-        logger.info("type={0}, latlon={1}, zoom={2}".format(types, latlon, zoom))
+        latlon = None
+        if "latlon" in event["queryStringParameters"]:
+            latlon = event["queryStringParameters"]["latlon"]
+        address = None
+        if "address" in event["queryStringParameters"]:
+            address = event["queryStringParameters"]["address"]
+        zoom = 16
+        if "zoom" in event["queryStringParameters"]:
+            zoom = int(event["queryStringParameters"]["zoom"])
+        count = RESULT_COUNT_MAX
+        if "count" in event["queryStringParameters"]:
+            count = int(event["queryStringParameters"]["count"])
+        isSort = False
+        if "sort" in event["queryStringParameters"]:
+            isSort = str2bool(event["queryStringParameters"]["sort"])
+        
+        if not latlon and address:
+            latlon = getLatLonFromAddress(address)
 
-        hits = search_h3(types, latlon, zoom)
+        message = "type={0}, latlon={1}, address={2}, zoom={3}, count={4}, sort={5}".format(types, latlon, address, zoom, count, isSort)
+        logger.info(message)
+        
+        hits = search_h3(types, latlon, zoom, count, isSort)
 
         result = {}
         has_clowd = False
@@ -81,7 +104,25 @@ def lambda_handler(event, context):
             "body": "error"
         }
 
-def search_h3(types, latlon, zoom):
+@retry(tries=3, delay=1)
+def getLatLonFromAddress(address):
+    try:
+        latlon = None
+        result = GMAPS.geocode(address)
+        try:
+            logger.info(result)
+            lat = result[0]["geometry"]["location"]["lat"]
+            lng = result[0]["geometry"]["location"]["lng"]
+            latlon = "{0},{1}".format(lat, lng)
+        except Exception as e:
+            logger.warn("can't get geocode : {0}".format(address))
+            
+        return latlon
+    except Exception as e:
+        logger.error(address)
+        logger.exception(e)
+
+def search_h3(types, latlon, zoom, count, isSort):
     if zoom <= 13:
         h3type = 7
     elif zoom >= 14 and zoom <= 15:
@@ -95,15 +136,34 @@ def search_h3(types, latlon, zoom):
     logger.info(rings)
 
     results = []
+    ringIndex = 0
     for sdata in rings:
         for h3index in sdata:
             response = query_dynamodb(types, h3index, h3type)
             results.extend(response)
-        if len(results) >= RESULT_COUNT_MAX:
+        logger.info("ringIndex={0}, resultCount={1}".format(ringIndex, len(results)))
+        if len(results) >= count and ringIndex > 0:
             break
+        ringIndex = ringIndex + 1
+        
     logger.info("result size = {0}".format(len(results)))
 
+    results = map(lambda data: calcDistanceFrom(latlon, data), results)
+    results = list(results)
+    
+    if isSort:
+        results = sorted(results, key=lambda data: data["distance"])
+        if count > 0 and len(results) > count:
+            results = results[0 : count]
+    
     return results
+
+def calcDistanceFrom(latlon, data):
+    latlon_ary1 = latlon.split(",")
+    latlon_ary2 = data["latlon"].split(",")
+    distance = geodesic(latlon_ary1, latlon_ary2).km
+    data["distance"] = distance
+    return data
 
 @retry(tries=3, delay=1)
 def query_dynamodb(types, h3index, h3type):
@@ -146,10 +206,12 @@ def getCrowdLvFromLoco(resultlist, locolist):
             ids += "{0},".format(item["locoguide_id"])
         ids.rstrip(",")
             
-        url = LOCOGUIDE_API_ADDRESS + "?id=" + ids
+        # url = LOCOGUIDE_API_ADDRESS + "?id=" + ids
+        url = LOCOGUIDE_API_ADDRESS2 + "?place_id=" + ids
+        
         idList = []
         lvList = []
-        has_clowd = requestLoco(url, 1, idList, lvList)
+        has_clowd = requestLoco2(url, 1, idList, lvList)
 
         for i in range(len(idList)):
             id = idList[i]
@@ -161,6 +223,50 @@ def getCrowdLvFromLoco(resultlist, locolist):
 
     except Exception as e:
         logger.exception(e)
+
+    return has_clowd
+
+
+def requestLoco2(url, page, idList, lvList):
+    logger.info("------loco address2------page=" + str(page))
+    logger.info(url)
+    headers ={}
+    headers["Authorization"] = "Bearer " + LOCOGUIDE_API_TOKEN
+    response = request(url, headers)
+    response.encoding = response.apparent_encoding
+    content = response.content.decode("utf-8")
+    jsn = json.loads(content)
+    
+    logger.info("list size = " + str(len(jsn)))
+    logger.info(jsn)
+
+    has_clowd = False
+    for tmp in jsn:
+        if "crowd_status" not in tmp or not tmp["crowd_status"]:
+            continue
+        color = tmp["crowd_status"]["color"]
+        
+        
+        if tmp["crowd_status"] != None:
+            color = tmp["crowd_status"]["color"]
+        lv = 0
+        if color == "red":
+            lv = 3
+        elif color == "yellow":
+            lv = 2
+        elif color == "green" or color == "blue":
+            lv = 1
+        lvList.append(lv)
+        idList.append(str(tmp["place_id"]))
+        has_clowd = True
+
+    if "Link" in response.headers:
+        nextUrl = response.headers["Link"]
+        nextUrl = nextUrl[1:nextUrl.find(">")]
+        page = page + 1
+        flg = requestLoco(nextUrl, page, idList, lvList)
+        if flg:
+            has_clowd = True
 
     return has_clowd
 
@@ -227,6 +333,7 @@ def convert(types, item):
     child["title"] = item["title"]
     child["tel"] = item["tel"]
     child["address"] = item["address"]
+    child["distance"] = item["distance"]
 
     if "image" in item and item["image"]:
         child["image"] = urljoin("https://near-near-map.s3-ap-northeast-1.amazonaws.com/", item["image"])
@@ -274,3 +381,6 @@ def convert(types, item):
     data["list"].append(child)
     
     return data
+    
+def str2bool(s):
+     return s.lower() in ["true", "t", "yes", "1"]
